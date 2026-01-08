52c6c7447e205a25b43d78597f15a5fb858d708e
Jacey <jaceyopara@gmail.com>
2026-01-08 23:19:01 +0100

    adjusted height of footer child elements on mobile

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 // Get all share buttons (both main and close button)
-        const shareButtons = document.querySelectorAll('.share--button');
-        const shareBox = document.getElementById('shareBox');
-        const authorBlock = document.getElementById('authorBlock');
+const shareButtons = document.querySelectorAll('.share--button');
+const shareBox = document.getElementById('shareBox');
+const authorBlock = document.getElementById('authorBlock');
 
-        // Toggle function
-        const toggleShare = () => {
-            
-        const isOpen = shareBox.classList.contains('active');
-        shareBox.classList.toggle('active');
-        authorBlock.classList.toggle('hidden', !isOpen);
-        };
+// Toggle function
+const toggleShare = () => {
+    const isOpen = shareBox.classList.contains('active');
+    shareBox.classList.toggle('active');
+    authorBlock.classList.toggle('hidden', !isOpen);
+}; 
 
-        // Add click listener to all share buttons
-        shareButtons.forEach(button => {
-        button.addEventListener('click', toggleShare);
-    });
+// Add click listener to all share buttons
+shareButtons.forEach(button => {
+    button.addEventListener('click', toggleShare);
+});
